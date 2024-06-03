f2f1fdd370039f97c9204e3917d198345fdc0629
Diamondijah <hadiadaoudadrame@gmail.com>
2024-06-03 15:54:31 +0000

    Premier depot : systeme d'authentification complete, la securité est a revoir biensur

---
+++ comptes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from comptes import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('register', views.register, name="register"),
+    path('login', views.logIn, name="login"),
+    path('logout', views.logOut, name="logout"),
+    path('activate/<uidb64>/<token>', views.activate, name="activate"),
+ 
+]
